2291f4c750a33511c752a0614293cc02c9011aa2
FIlipHand <filif.reka@gmail.com>
2022-11-10 22:28:48 +0100

    lab4 pisanie testów in progress

---
+++ src/test/java/SimulationTest.java
@@ -0,0 +1,15 @@
+import agh.ics.oop.*;
+import org.junit.jupiter.api.Test;
+
+public class SimulationTest {
+
+    @Test
+    public void testSimulation() {
+        String[] args = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
+        MoveDirection[] directions = new OptionsParser().parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = {new Vector2d(2, 2), new Vector2d(3, 4)};
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+    }
+}
